e28b4ab52a048b15c76843f83b65cad52d7d4e28
sohimaster <sohimaster@ya.ru>
2020-03-07 17:45:54 +0500

    remove old code

--- src/router.py
+++ src/router.py
@@ -8,22 +8,4 @@ class Router:
         app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
         app.add_url_rule('/register', 'register', register, methods=['GET', 'POST'])
         app.add_url_rule('/logout', 'logout', logout, methods=['GET', 'POST'])
-
-        # app.add_url_rule('/automatching',
-        #                  'automatching',
-        #                  view_func=AutoMatcher.as_view('automatcher'))
-        # app.add_url_rule('/filematching',
-        #                  'filematching',
-        #                  view_func=FileMatcher.as_view('filematcher'))
-        # app.add_url_rule('/clients',
-        #                  'clients',
-        #                  view_func=Client.as_view('clients'))
-        # app.add_url_rule('/products',
-        #                  'products',
-        #                  view_func=Product.as_view('products'))
-        # app.add_url_rule('/matching',
-        #                  'matching',
-        #                  view_func=Matching.as_view('matching'))
-        #
-        # app.add_url_rule('/csv', 'csv', send_csv, methods=['GET'])
         return app
