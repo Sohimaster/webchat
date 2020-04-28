a040c74ee0878a2759ac9163a0457016a8ce0537
sohimaster <sohimaster@ya.ru>
2020-04-28 15:46:09 +0500

    bugfix for unselected chat preview update, and first chat is selected by default

--- app/views/static/js/main.js
+++ app/views/static/js/main.js
@@ -16,6 +16,13 @@ for (let _chat of chats) {
     socket.emit('join', chat_id);
 }
 
+window.onload = function() {
+    let first_person = document.querySelector('.person');
+    if (first_person) {
+        setAciveChat(first_person);
+    }
+};
+
 console.log(chat);
 function setAciveChat(element) {
     let active_person = document.querySelector('.active');
@@ -40,41 +47,47 @@ function setAciveChat(element) {
     wrapper.scrollTo(0,wrapper.scrollHeight);
 }
 
+function getDatetime(){
+    let today = new Date();
+    return today.getFullYear() + "-" +
+        ('0' + (today.getMonth() + 1)) + "-" +
+        ('0' + today.getDate()).slice(-2) + " " +
+        ('0' + today.getHours()).slice(-2) + ":" +
+        ('0' + today.getMinutes()).slice(-2) + ":" +
+        ('0' + today.getSeconds()).slice(-2);
+}
+
 // Send message
 let message_input = document.getElementById("send_message");
 message_input.onkeypress = async function(event){
-        if (event.keyCode == 13 || event.which == 13){
-            let message = message_input.value;
-            if (!message) {
-                return;
-            }
-            let active_person = document.querySelector('.active');
-            let preview = active_person.querySelector('.preview');
-            let chat_id = active_person.getAttribute('data-chat');
-            let active_chat = chat.container
-                .querySelector('[data-chat="' + chat_id + '"]');
-            let wrapper = active_chat.querySelector('.chat_wrapper');
-            let today = new Date();
-            let datetime = today.getFullYear() + "-" +
-                ('0' + (today.getMonth() + 1)) + "-" +
-                ('0' + today.getDate()).slice(-2) + " " +
-                ('0' + today.getHours()).slice(-2) + ":" +
-                ('0' + today.getMinutes()).slice(-2) + ":" +
-                ('0' + today.getSeconds()).slice(-2);
-            console.log(datetime);
-            socket.emit('message', datetime, message, chat_id);
-            message_input.value = '';
-            wrapper.classList.add('notransition');
-            for (let element of wrapper.children) {
-                element.classList.add('notransition');
-            }
-            wrapper.innerHTML += `<div class="bubble me notransition">
-                                        ${message}
-                                  </div>`;
-            preview.innerHTML = message;
-            wrapper.scrollTo(0,wrapper.scrollHeight);
+    if (event.keyCode == 13 || event.which == 13){
+        let message = message_input.value;
+        if (!message) {
+            return;
         }
-    };
+        let active_person = document.querySelector('.active');
+        let preview = active_person.querySelector('.preview');
+        let chat_id = active_person.getAttribute('data-chat');
+        let active_chat = chat.container
+            .querySelector('[data-chat="' + chat_id + '"]');
+        let wrapper = active_chat.querySelector('.chat_wrapper');
+
+        let datetime = getDatetime();
+        console.log(datetime);
+
+        socket.emit('message', datetime, message, chat_id);
+        message_input.value = '';
+        wrapper.classList.add('notransition');
+        for (let element of wrapper.children) {
+            element.classList.add('notransition');
+        }
+        wrapper.innerHTML += `<div class="bubble me notransition">
+                                    ${message}
+                              </div>`;
+        preview.innerHTML = message;
+        wrapper.scrollTo(0,wrapper.scrollHeight);
+    }
+};
 
 
 // Get message
@@ -85,8 +98,10 @@ socket.on( 'render_message', function( json_data ) {
     //     'datetime',
     //     'message'
     // }
-    let active_person = document.querySelector('.active');
-    let preview = active_person.querySelector('.preview');
+    let person = document
+        .querySelector('.left')
+        .querySelector('[data-chat="' + json_data['chat_id'] + '"]');
+    let preview = person.querySelector('.preview');
     let user_id = document.getElementById('user_id').value;
     let sender_id = json_data['sender_id'];
     let message = json_data['message'];
